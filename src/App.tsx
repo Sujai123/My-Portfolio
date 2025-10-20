ed6c7eb46d02145741a64f01483e262e7b8c31d7
Sujai123 <sujaibeniks001@gmail.com>
2025-10-20 18:30:38 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import './globals.css';
 import useSmoothScroll from "./hooks/useSmoothScroll";
 
 function App() {
   useSmoothScroll();
 
   return (
-    <>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
