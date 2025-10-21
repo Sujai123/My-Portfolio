cff7b1dd04a4f55600310c855a7ddcf700893c6b
Sujai123 <sujaibeniks001@gmail.com>
2025-10-21 19:52:11 +0530

    integrate 2nd app

---
+++ apps/app-two/src/App.tsx
@@ -0,0 +1,11 @@
+import Button from "@common/Button";
+import "./globals.css";
+import useSmoothScroll from "@common/hooks/useSmoothScroll";
+
+function App() {
+  useSmoothScroll();
+
+  return <Button />;
+}
+
+export default App;
